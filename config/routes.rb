a2af92b13dba80520363988e86e623ba615935ae
Natalisp <nathalipat@gmail.com>
2017-03-21 17:51:40 -0400

    routes api

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: 'json'} do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1) do
+    scope module: :v1 do
       resources :people
     end
   end
