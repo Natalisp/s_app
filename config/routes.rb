614e8662a4e31e0ecc8be83506ce3580eace8f5c
Natalisp <nathalipat@gmail.com>
2017-03-21 22:49:32 -0400

    spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ require 'api_constraints'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: {format: 'json'} do
-    scope module: :v1 do
-      resources :people
-    end
+namespace :api, defaults: {format: 'json'} do
+  scope module: :v1 do
+    resources :people
   end
+end
 
 resources :people
 root 'people#index'
